bffd4031ec8961a3ea9bffecde30653ce8ee88ff
Demian Semenov <semenovdemyan@gmail.com>
2026-01-29 18:51:18 +0300

    init commit

---
+++ MemoCardApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MemoCardApp
+//
+//  Created by Demian on 28.01.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
